60ab08c83cb02a79c14d7c9fc881fef4eb069e58
Irodabonu <111131989+Irodabonu@users.noreply.github.com>
2023-05-15 12:04:15 +0500

    Create Introduction.rb
    Ruby is a brilliant language

---
+++ Introduction.rb
@@ -0,0 +1,6 @@
+def class_obj(objects, place)
+    puts "Hello My name is " + objects + " So I am studying at Software engineering"
+    puts "At " + place
+end
+
+class_obj("Irodabonu", "Astrum IT academy")
